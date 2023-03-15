1b2e65d9e6ab89d9b13f9b6a2a2243c9f70cb815
sansavenir <jakub_kotal@hotmail.com>
2023-03-15 15:50:24 +0100

    new view

--- lib/amphi_web/controllers/search_controller.ex
+++ lib/amphi_web/controllers/search_controller.ex
@@ -2,6 +2,14 @@ defmodule AmphiWeb.SearchController do
   use AmphiWeb, :controller
   # use Floki
 
+  def concat_text(text_list) do
+    text_list |> Enum.map(fn x -> Enum.map(x, fn
+        {"b", _, a} -> a |> Enum.at(0)
+        {"br", _, a} -> ""
+        x -> x
+      end)|> Enum.join("") end)
+  end
+
   def index(conn, command) do
     api_key = "0219851630701e8bf03ad9fd44d1378b"
 
@@ -22,15 +30,15 @@ defmodule AmphiWeb.SearchController do
         {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
             html_body = body
             elements = html_body |> Floki.find("div.gs_r.gs_or.gs_scl") |> Floki.find("h3") |> Floki.find("a")
+            description = html_body |> Floki.find("div.gs_rs") |> Enum.map(fn x -> x |> elem(2) end)
+            description = concat_text(description)
 
             names = Enum.map(elements, fn x -> x |> elem(2) end)
             links = Enum.map(elements, fn x -> x |> elem(1) |> Enum.at(1) |> elem(1) end)
-            names = names |> Enum.map(fn x -> Enum.map(x, fn
-                {"b", _, a} -> a |> Enum.at(0)
-                x -> x
-              end)|> Enum.join("") end)
+            names = concat_text(names)
 
-            results = Enum.zip(names, links) |> Enum.map(fn {x, y} -> %{name: x, link: y} end)
+            results = Enum.zip([names, links, description]) |> Enum.map(fn {x, y, z} -> %{name: x, link: y, description: z} end)
+
 
             render(conn, :index, results: results)
       end
