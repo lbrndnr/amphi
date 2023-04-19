679ba49e1e6b58d62877a7d8f405734bb6c6341b
Laurin Brandner <mail@laurinbrandner.ch>
2023-04-19 21:47:43 +0200

    Fix css

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -5,7 +5,6 @@ import * as pdfjsViewer from 'pdfjs-dist/web/pdf_viewer'
 pdfjsLib.GlobalWorkerOptions.workerSrc = '../assets/pdf.worker.js'
 
 window.onload = async () => {
-
 	const container = document.querySelector('#pdf-wrapper');
 
 	const url = "https://arxiv.org/pdf/2001.01653.pdf";
@@ -35,60 +34,14 @@ window.onload = async () => {
 	pdfViewer.setPdfPage(page);
 	linkService.setViewer(pdfViewer);
 
+	document.addEventListener("selectionchange", onSelectionChange);
+
 	return pdfViewer.draw();
 }
 
-// window.onload = async () => {
-// 	var url = "https://arxiv.org/pdf/2001.01653.pdf";
-// 	const loadingTask = pdfjsLib.getDocument(url);
-// 	const pdf = await loadingTask.promise;
+function onSelectionChange(event) {
+	const sel = document.getSelection();
+	console.log(sel.toString());
 
-// 	// Load information from the first page.
-// 	const page = await pdf.getPage(1);
-
-	// const params = {
-	// 	scale: window.devicePixelRatio
-	// };
-	// const viewport = page.getViewport(params);
-
-// 	// Apply page dimensions to the `<canvas>` element.
-// 	const canvas = document.getElementById('pdf-canvas');
-// 	const context = canvas.getContext('2d');
-// 	canvas.height = viewport.height;
-// 	canvas.width = viewport.width;
-
-// 	// Render the page into the `<canvas>` element.
-// 	const renderContext = {
-// 		canvasContext: context,
-// 		viewport: viewport,
-// 	};
-
-// 	// await page.render(renderContext);
-// 	const renderTask = page.render(renderContext);
-
-// 	// Wait for rendering to finish
-//     renderTask.promise.then(function() {
-// 		// Returns a promise, on resolving it will return text contents of the page
-// 		return page.getTextContent();
-// 	}).then(function(textContent) {  
-// 		// Assign CSS to the textLayer element
-// 		const textLayer = document.getElementById('text-layer');
-  
-// 		// textLayer.scale = window.devicePixelRatio;
-// 		// textLayer.height = viewport.height;
-// 		// textLayer.width = viewport.width;
-
-// 		textLayer.style.left = canvas.offsetLeft + 'px';
-// 		textLayer.style.top = canvas.offsetTop + 'px';
-// 		textLayer.style.height = canvas.offsetHeight + 'px';
-// 		textLayer.style.width = canvas.offsetWidth + 'px';
-  
-// 		// Pass the data to the method for rendering of text over the pdf canvas.
-// 		pdfjsLib.renderTextLayer({
-// 		  textContentSource: textContent,
-// 		  container: textLayer,
-// 		  viewport: viewport,
-// 		  textDivs: []
-// 		});
-// 	});
-// }
+	
+}
