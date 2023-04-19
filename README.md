21b507a7b9959dd1c5b054cb8a7be9eb51ce4626
Laurin Brandner <mail@laurinbrandner.ch>
2023-04-19 22:44:36 +0200

    Update assets.build

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 To start the server, run
 
 ```
-mix setup #to install and setup dependencies
+mix setup # to install and setup dependencies
 docker run --rm --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/Documents/Docker/postgres:/var/lib/postgresql/data postgres # to run postgres docker
 docker run -it -p 8050:8050 scrapinghub/splash --max-timeout 300 # to run splash (headless browser)
 mix ecto.migrate # if any outstanding migrations
--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule Amphi.MixProject do
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["tailwind default", "esbuild default"],
+      "assets.build": ["npm install --prefix assets", "tailwind default", "esbuild default"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
