b48852b38225ad947bda5c84c6ae22037172d299
Laurin Brandner <mail@laurinbrandner.ch>
2023-05-09 14:37:19 +0200

    Multi page pdf using PDFPageView

--- assets/js/hooks/pdf-viewer.js
+++ assets/js/hooks/pdf-viewer.js
@@ -3,30 +3,55 @@ import * as pdfjsViewer from 'pdfjs-dist/web/pdf_viewer';
 
 const PDFViewer = {
 	mounted() {	
-		this.containerElement = this.el.querySelector("#pdf-container");
-		this.viewerElement = this.el.querySelector(".pdfViewer");
-		this.url = this.containerElement.getAttribute("pdf_url");
+		this.url = this.el.getAttribute("pdf-url");
 
-		this.initPDFViewer();
 		pdfjsLib.getDocument(this.url).promise.then((pdf) => {
-			this.viewer.setDocument(pdf);
+			this.pdf = pdf;
+			this.initContainers();
+			for (let i = 0; i < this.pdf.numPages; i++) {
+				this.loadPage(i);
+			}
+		});
+
+		document.addEventListener("selectionchange", (event) => {
+			const sel = document.getSelection();
+			const range = sel.getRangeAt(0); 
+			const rect = range.getBoundingClientRect();
+
+			console.log(range, rect);
 		});
 	},
-	initPDFViewer() {
-		const eventBus = new pdfjsViewer.EventBus();
-		const linkService = new pdfjsViewer.PDFLinkService({
-			eventBus,
-		});	
-		const container = this.containerElement;
-		const viewer = this.viewerElement;
-		this.viewer = new pdfjsViewer.PDFViewer({
-			container,
-			viewer,
-			eventBus,
-			linkService,
-			l10n: pdfjsViewer.NullL10n
+	initContainers() {
+		this.containers = Array(this.pdf.numPages);
+		for (let i = 0; i < this.pdf.numPages; i++) { 
+			const elem = document.createElement("div");
+			elem.setAttribute("class", "pdfViewer");
+			this.el.appendChild(elem);
+
+			this.containers[i] = elem;
+		  }		
+	},
+	loadPage(idx) {
+		this.pdf.getPage(idx+1).then((page) => {
+			const eventBus = new pdfjsViewer.EventBus();
+			const linkService = new pdfjsViewer.PDFLinkService({
+				eventBus,
+			});	
+			const viewport = page.getViewport({
+				scale: window.devicePixelRatio
+			});
+			const container = this.containers[idx];
+			this.viewer = new pdfjsViewer.PDFPageView({
+				container,
+				eventBus,
+				linkService,
+				l10n: pdfjsViewer.NullL10n,
+				defaultViewport: viewport
+			});
+			this.viewer.setPdfPage(page);
+			this.viewer.draw();
+			linkService.setViewer(this.viewer);
 		});
-		linkService.setViewer(this.viewer);
 	}
 }
 
